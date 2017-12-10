fb9cea45af74c28c6b704537cd6d8df56cf064a6
Calvin Sant <calvinsant@gmail.com>
2017-12-10 00:23:40 -0700

    Instruction and remaining gulp modules

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gulp-template
 Ongoing template for web development code optimization
+
+1.  Download and install Node.js and NPM here - https://nodejs.org/en/download/
+2.  During the install, select the second option that includes "npm".
+3.  Place the "gulp-template" directory above the root of the project.
+4.  Install gitbash from here - https://git-scm.com/downloads
+5.  Within gitbash, CD to project "gulp-template" and run "npm install".
+6.  Configure gulp.js directories to match the current project.
+7.  Within gitbash run "gulp watch".
+
+Now, all of your specified style and js files will automatically minified, concatinated and exported to your specified location in your projects root directory upon save!
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
-	minifyCss = require('gulp-minify-css');
+	minifyCss = require('gulp-clean-css'),
+	sourceMaps = require('gulp-sourcemaps');
 
 gulp.task('default', function(){
-	console.log('look at my first gulp task!');
+	gulp.run('js','css');
 });
 
 gulp.task('css',function(){
 	//Get css files, minify, concatonate and output
 	//Comma separate in src for multiple
 	gulp.src(['./src/css/*.css'])
+	.pipe(sourceMaps.init())
 	.pipe(minifyCss())
 	.pipe(concat('style.css'))
+	.pipe(sourceMaps.write())
 	.pipe(gulp.dest('./dist/css/'));
-})
+});
 gulp.task('js', function(){
 	gulp.src(['./src/js/*.js'])
 	.pipe(uglify())
 	.pipe(concat('script.js'))
 	.pipe(gulp.dest('./dist/js/'));
-})
+});
+
+gulp.task('watch', function(){
+	gulp.run('default');
+	gulp.watch('./src/css/*', function(){
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('css');
+	});
+	gulp.watch('./src/js/*', function(){
+		console.log('File ' + event.path + ' was ' + event.type + ', running task...');
+	});
+});
